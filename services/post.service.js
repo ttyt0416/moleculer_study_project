818a6932f1a61098ce6a96f2e3ffa910ed78559e
yoon <ttyt0416@gmail.com>
2024-03-23 13:20:16 +0900

    add comment crud

--- services/post.service.js
+++ services/post.service.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const DbMixin = require("../mixins/db.mixin");
+const { MoleculerClientError } = require("moleculer").Errors;
 
 /**
  * @typedef {import('moleculer').ServiceSchema} ServiceSchema Moleculer's Service Schema
@@ -26,6 +27,7 @@ module.exports = {
 			"_id",
 			"title",
 			"body",
+			"comments",
 			"createdAt",
 			"updatedAt",
 		],
@@ -69,6 +71,7 @@ module.exports = {
 					const inserted = await this.adapter.insert({
 						title,
 						body,
+						comments: [],
 						createdAt: new Date(),
 						updatedAt: new Date(),
 					})
@@ -78,13 +81,128 @@ module.exports = {
 				}
 			},
 		},
+
+		createComment: {
+			rest: "POST /create-comment",
+			params: {
+				postId: "string",
+				body: "string",
+			},
+			async handler(ctx) {
+				const {postId, body} = ctx.params
+
+				const foundPost = await this.adapter.findById(postId)
+
+				if (!foundPost) {
+					throw new MoleculerClientError("post_not_valid");
+				}
+
+				const result = await this.adapter.updateById(postId, {
+					$set: {
+						comments: [...foundPost.comments, {
+							id: this.generateId(20, false),
+							body: body,
+							createdAt: new Date(),
+							updatedAt: new Date(),
+						}],
+					}
+				})
+				return result;
+			},
+		},
+
+		updateComment: {
+			rest: "POST /update-comment",
+			params: {
+				postId: "string",
+				commentId: "string",
+				body: "string",
+			},
+			async handler(ctx) {
+				const {postId, commentId, body} = ctx.params
+
+				const foundPost = await this.adapter.findById(postId)
+
+				if (!foundPost) {
+					throw new MoleculerClientError("post_not_valid");
+				}
+
+				const foundCommentIndex = foundPost.comments.findIndex((el) => el.id === commentId);
+
+				if (!foundCommentIndex < 0) {
+					throw new MoleculerClientError("comment_not_found");	
+				}
+
+				const updatedComments = [
+					...foundPost.comments.slice(0, foundCommentIndex),
+					{
+						id: foundPost.comments[foundCommentIndex].id,
+						body: body,
+						createdAt: foundPost.comments[foundCommentIndex].createdAt,
+						updatedAt: new Date(),
+					},
+					...foundPost.comments.slice(foundCommentIndex + 1, foundPost.comments.length)
+				]
+
+				const result = await this.adapter.updateById(postId, {
+					$set: {
+						comments: updatedComments,
+					}
+				})
+				return result;
+			},
+		},
+
+		deleteComment: {
+			rest: "POST /delete-comment",
+			params: {
+				postId: "string",
+				commentId: "string",
+			},
+			async handler(ctx) {
+				const {postId, commentId} = ctx.params
+
+				const foundPost = await this.adapter.findById(postId)
+
+				if (!foundPost) {
+					throw new MoleculerClientError("post_not_valid");
+				}
+
+				const foundCommentIndex = foundPost.comments.findIndex((el) => el.id === commentId);
+
+				if (!foundCommentIndex < 0) {
+					throw new MoleculerClientError("comment_not_found");	
+				}
+
+				const updatedComments = foundPost.comments.filter((comment) => comment.id !== commentId);
+
+				const result = await this.adapter.updateById(postId, {
+					$set: {
+						comments: updatedComments,
+					}
+				})
+				return result;
+			},
+		},
 	},
+
+
 
 	/**
 	 * Methods
 	 */
 	methods: {
-		
+		generateId(length, easyRead) {
+			var result           = '';
+			var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+			if(easyRead)
+					characters  = 'abcdefghijklmnpqrstuvwxyz123456789'; //Remove confusing characters like 0 and O
+			var charactersLength = characters.length;
+			for ( var i = 0; i < length; i++ ) {
+				 result += characters.charAt(Math.floor(Math.random() * charactersLength));
+			}
+			return result;
+	 }
 	},
 
 	/**
